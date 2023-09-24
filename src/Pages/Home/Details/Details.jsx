62b01e888eb699fbc4b739ac71c3bde6b783bd5c
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-09-25 05:40:14 +0600

    details page added

--- src/Pages/Home/Details/Details.jsx
+++ src/Pages/Home/Details/Details.jsx
@@ -10,10 +10,28 @@ const Details = () => {
     setData(findData);
   }, [id, allData]);
   console.log(data);
-  const { image, title, description, text_color, amount } = data;
+  const { image_2, title, description, text_color, amount } = data;
+  const btnStyle = {
+    margin: "50px 40px",
+    padding: "16px 26px",
+    backgroundColor: text_color,
+    color: "#fff",
+    fontWeight: 600,
+    fontSize: 20,
+    borderRadius: 6,
+  };
   return (
     <div>
-      <img src="./../" alt="" />
+      <div>
+        <img className="w-full h-[700px] rounded-lg" src={image_2} alt="" />
+        <div className="bg-black bg-opacity-30 absolute w-[1320px] -bottom-[90px] rounded-b-lg">
+          <button style={btnStyle}>Donate ${amount}</button>
+        </div>
+      </div>
+      <div className="my-10 space-y-6">
+        <h3 className="text-5xl font-bold">{title}</h3>
+        <p className="text-lg">{description}</p>
+      </div>
     </div>
   );
 };
