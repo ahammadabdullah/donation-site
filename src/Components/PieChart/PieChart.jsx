d89dba24a81797bb1e860a61736a979220d9883f
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-09-26 14:29:32 +0600

    fix: pieChart

---
+++ src/Components/PieChart/PieChart.jsx
@@ -0,0 +1,74 @@
+import { PureComponent } from "react";
+
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+
+const Piechart = () => {
+  const donationList = JSON.parse(localStorage.getItem("donations"));
+  let myDonation = 0;
+  if (donationList) {
+    myDonation = myDonation + donationList.length;
+  }
+  const totalDonation = 20;
+  const data = [
+    { name: "Total Donation", value: totalDonation },
+    { name: "Your Donation", value: myDonation },
+  ];
+  const COLORS = ["#0088FE", "#00C49F"];
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <PieChart width={600} height={600}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Legend verticalAlign="center" align="center" />
+        <Tooltip />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default Piechart;
--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,38 +1,12 @@
 /* eslint-disable no-undef */
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie } from "react-chartjs-2";
-ChartJS.register(ArcElement, Tooltip, Legend);
+
+import Piechart from "../../Components/PieChart/PieChart";
 
 const Statistics = () => {
-  const donationList = JSON.parse(localStorage.getItem("donations"));
-  let myDonation = 0;
-  if (donationList) {
-    myDonation = myDonation + donationList.length;
-  }
-  const totalDonation = 20;
-  const data = {
-    labels: ["Your Donation", "Total Donation"],
-    datasets: [
-      {
-        data: [myDonation, totalDonation],
-        backgroundColor: [
-          "rgb(255, 99, 132)",
-          "rgb(54, 162, 235)",
-          "rgb(255, 205, 86)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="flex w-[350] md:w-[500px]">
-        <Pie data={data} />
+    <div>
+      <div className="h-[400px] w-[600px] mx-auto flex justify-center items-center">
+        <Piechart></Piechart>
       </div>
     </div>
   );
